10775083658488a2a053ef02f8e5dbf26792b614
seluce <36071735+seluce@users.noreply.github.com>
2019-04-14 12:08:32 +0200

    Update n_asb_01-2018.sh

--- n_asb_01-2018.sh
+++ n_asb_01-2018.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ../../../../..
-patch -p1 < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_01-2018/af105ed.patch
+patch -p1 < device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_01-2018/af105ed.patch
 cd build
 patch -p1 < ../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_01-2018/build/f057915.patch
 cd ../../..
@@ -16,22 +16,22 @@ cd ../..
 cd system/core
 patch -p1 < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_01-2018/system_core/50ec193.patch
 cd ../..
-cd frameworks_ex
+cd frameworks/ex
 patch -p1 < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_01-2018/frameworks_ex/61e42c3.patch
 cd ../..
-cd external_libvpx
+cd external/libvpx
 patch -p1 < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_01-2018/external_libvpx/7aacdba.patch
 cd ../..
-cd external_libmpeg2
+cd external/libmpeg2
 patch -p1 < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_01-2018/external_libmpeg2/d00e1f4.patch
 cd ../..
-cd frameworks_base
+cd frameworks/base
 patch -p1 < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_01-2018/frameworks_base/5a280fb.patch
 patch -p1 < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_01-2018/frameworks_base/51e18df.patch
 patch -p1 < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_01-2018/frameworks_base/ad38167.patch
 patch -p1 < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_01-2018/frameworks_base/e4c0c23.patch
 cd ../..
-cd frameworks_av
+cd frameworks/av
 patch -p1 < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_01-2018/frameworks_av/8cb6cdd.patch
 patch -p1 < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_01-2018/frameworks_av/10aadb1.patch
 patch -p1 < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_01-2018/frameworks_av/19bcd55.patch
@@ -41,12 +41,12 @@ patch -p1 < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_01-2018/frameworks_
 patch -p1 < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_01-2018/frameworks_av/c0710cf.patch
 patch -p1 < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_01-2018/frameworks_av/cb33b28.patch
 cd ../..
-cd external_aac
+cd external/aac
 patch -p1 < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_01-2018/external_aac/c02dd94.patch
 patch -p1 < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_01-2018/external_aac/d807ebc.patch
 patch -p1 < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_01-2018/external_aac/d807ebc.patch
 cd ../..
-cd external_libhevc
+cd external/libhevc
 patch -p1 < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_01-2018/external_libhevc/2a44132.patch
 patch -p1 < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_01-2018/external_libhevc/03d6532.patch
 patch -p1 < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_01-2018/external_libhevc/5defac3.patch
