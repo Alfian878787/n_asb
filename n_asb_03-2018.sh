a971c18ebc82d05ca95ba4c240e8cc276942839e
seluce <36071735+seluce@users.noreply.github.com>
2019-04-14 14:59:41 +0200

    Update n_asb_03-2018.sh

--- n_asb_03-2018.sh
+++ n_asb_03-2018.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 cd ../../../..
 cd build
-patch -p1 --forward < ../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/build/7293cf7.patch
-patch -p1 --forward < ../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/build/e3c263e.patch
+git appy --check < ../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/build/7293cf7.patch
+git appy --check < ../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/build/e3c263e.patch
 git clean -f -d
 cd ..
 cd external/libavc
-patch -p1 --forward < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/external_libavc/04c9906.patch
-patch -p1 --forward < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/external_libavc/db6bf49.patch
+git appy --check < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/external_libavc/04c9906.patch
+git appy --check < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/external_libavc/db6bf49.patch
 git clean -f -d
 cd ../..
 cd external/libmpeg2
-patch -p1 --forward < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/external_libmpeg2/98beb25.patch
-patch -p1 --forward < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/external_libmpeg2/6337ea6.patch
-patch -p1 --forward < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/external_libmpeg2/d63479b.patch
+git appy --check < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/external_libmpeg2/98beb25.patch
+git appy --check < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/external_libmpeg2/6337ea6.patch
+git appy --check < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/external_libmpeg2/d63479b.patch
 git clean -f -d
 cd ../..
 cd frameworks/av
-patch -p1 --forward < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/frameworks_av/f059aa7.patch
+git appy --check < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/frameworks_av/f059aa7.patch
 git clean -f -d
 cd ../..
 cd system/bt
-patch -p1 --forward < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/system_bt/1b2d48c.patch
-patch -p1 --forward < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/system_bt/2f7820f.patch
-patch -p1 --forward < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/system_bt/3ee82f7.patch
-patch -p1 --forward < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/system_bt/9e46a39.patch
-patch -p1 --forward < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/system_bt/7705a32.patch
-patch -p1 --forward < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/system_bt/a5ececa.patch
-patch -p1 --forward < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/system_bt/a9c3c6d.patch
-patch -p1 --forward < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/system_bt/abab132.patch
-patch -p1 --forward < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/system_bt/c7c5824.patch
-patch -p1 --forward < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/system_bt/e0e8df0.patch
+git appy --check < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/system_bt/1b2d48c.patch
+git appy --check < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/system_bt/2f7820f.patch
+git appy --check < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/system_bt/3ee82f7.patch
+git appy --check < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/system_bt/9e46a39.patch
+git appy --check < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/system_bt/7705a32.patch
+git appy --check < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/system_bt/a5ececa.patch
+git appy --check < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/system_bt/a9c3c6d.patch
+git appy --check < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/system_bt/abab132.patch
+git appy --check < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/system_bt/c7c5824.patch
+git appy --check < ../../device/CUBOT/NOTE_PLUS/patches/n_asb/n_asb_03-2018/system_bt/e0e8df0.patch
 git clean -f -d
 cd ../..
