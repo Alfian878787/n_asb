42fa8774e3d21564bb63f4c2b09507447bf39cca
seluce <36071735+seluce@users.noreply.github.com>
2019-04-29 23:11:02 +0200

    Update apply-patches.sh

--- crdroid/apply-patches.sh
+++ crdroid/apply-patches.sh
@@ -1,32 +1,35 @@
+#!/bin/bash
+CURRENT_DIR=${PWD}
+
 echo -e "\n add security patches for CrDroid 3.8.9"
 sleep 1
 echo -e "\n start patching... wait some minutes"
 sleep 2
 
-bash n_asb_08-2018.sh
+. n_asb_08-2018.sh
 
-cd device/CUBOT/NOTE_S/patches/n_asb/crdroid
-bash n_asb_09-2018.sh
+cd $CURRENT_DIR
+. n_asb_09-2018.sh
 
-cd device/CUBOT/NOTE_S/patches/n_asb/crdroid
-bash n_asb_10-2018.sh
+cd $CURRENT_DIR
+. n_asb_10-2018.sh
 
-cd device/CUBOT/NOTE_S/patches/n_asb/crdroid
-bash n_asb_11-2018.sh
+cd $CURRENT_DIR
+. n_asb_11-2018.sh
 
-cd device/CUBOT/NOTE_S/patches/n_asb/crdroid
-bash n_asb_12-2018.sh
+cd $CURRENT_DIR
+. n_asb_12-2018.sh
 
-cd device/CUBOT/NOTE_S/patches/n_asb/crdroid
-bash n_asb_01-2019.sh
+cd $CURRENT_DIR
+. n_asb_01-2019.sh
 
-cd device/CUBOT/NOTE_S/patches/n_asb/crdroid
-bash n_asb_02-2019.sh
+cd $CURRENT_DIR
+. n_asb_02-2019.sh
 
-cd device/CUBOT/NOTE_S/patches/n_asb/crdroid
-bash n_asb_03-2019.sh
+cd $CURRENT_DIR
+. n_asb_03-2019.sh
 
-cd device/CUBOT/NOTE_S/patches/n_asb/crdroid
-bash n_asb_04-2019.sh
+cd $CURRENT_DIR
+. n_asb_04-2019.sh
 
 echo -e "\n security patch level: 2019-04-05"
